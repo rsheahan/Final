c42514dbaf3760662c8186af5212e69447f46393
rsheahan <charles.sheahan@my.ccsu.edu>
2018-12-03 13:15:00 -0500

    Update

--- boards/views.py
+++ boards/views.py
@@ -1,11 +1,30 @@
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from boards.models import User
 
-
-# Create your views here.
 
 def index(request):
 
     template = 'index.html'
 
     return render(request, template)
+
+def viewRegister(request):
+
+    return render(request, 'register.html')
+
+
+def addUser(request):
+
+    if request.method == 'POST':
+
+        firstName = request.POST.get("firstName")
+        lastName = request.POST.get("lastName")
+        age = request.POST.get("age")
+        email = request.POST.get("email")
+
+
+        user = User(first_name = firstName, last_name = lastName, age = age, email = email)
+        user.save()
+
+        return redirect('index')
