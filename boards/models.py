9c958046c690170cc14a0a0e9b1a04e754812710
rsheahan <charles.sheahan@my.cccs.edu>
2018-12-04 16:51:59 -0500

    update

--- boards/models.py
+++ boards/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 
-class User(models.Model):
+class user(models.Model):
     first_name = models.CharField(max_length=60)
     last_name = models.CharField(max_length=60)
     age = models.IntegerField()
